7ac9850c5884f4905999c1a16ff2f0ac43b81c09
Renan Florencio <renanflorencio689@gmail.com>
2022-09-18 13:09:17 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/one/digitalinnovation/peoplemanagement/PeoplemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.peoplemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
